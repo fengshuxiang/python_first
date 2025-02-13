b9313173bfce21ee6f73063a97820eac52b62fba
fsx <fsx_9999@163.com>
2025-02-13 22:08:35 +0800

    hello world

--- master/first.py
+++ master/first.py
@@ -1,3 +1,11 @@
-#first python program
-print("hello world")
-#this is a comment
+# 第一个 Python 程序
+# 定义一个函数，用于打印信息
+def print_message(message):
+    # 打印传入的字符串
+    print(message)
+
+# 这是一个注释
+
+if __name__ == "__main__":
+    # 打印 "hello world"
+    print_message("hello world")
